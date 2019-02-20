bed0604a86a1c06258a683b79178c7953cc0bee3
Furkan Mudanyalı <fmudanyali@icloud.com>
2019-02-20 14:08:15 +0300

    The latest example we did in class

---
+++ stuff/argument.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python3
+
+import math
+import sys
+
+print("This files name: ")
+print(sys.argv[0])
+print("Your first argument: ")
+print(sys.argv[1])
+print("Your second argument: ")
+print(sys.argv[2])
+print("Sqrt of the second argument: ")
+print(math.sqrt(int(sys.argv[2])))
