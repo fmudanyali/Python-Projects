6e2a57c27f4b2488e42b8d6fd2a32538edb44a53
Furkan Mudanyalı <fmudanyali@icloud.com>
2019-02-22 01:24:15 +0300

    changed variables with a and b, dunno why.

--- stuff/selectionsort.py
+++ stuff/selectionsort.py
@@ -4,21 +4,21 @@ import stdio
 toSort = open("tosort.txt","r+")
 toSort = toSort.read().strip("\n").split(",")
 toSort = list(map(int, toSort))
-#i starts at 0, gets added 1 each loop, continues until its equal to toSort's size
-for i in range(len(toSort)):
-    #set our current minimum to the value of i
-    currMin = i
-    #j starts at i+1, gets added 1 each loop, continues until its equal to toSort's size
-    for j in range(i+1,len(toSort)):
-        #if current minimumth element of toSort is bigger than jth element of toSort,
-        if toSort[currMin]>toSort[j]:
-            #set our current minimum to j
-            currMin = j
-    #replace current minumumth element with ith element
-    toSort[currMin],toSort[i]=toSort[i],toSort[currMin]
-#i starts at 0, gets added 1 each loop, continues until its equal to toSort's size
-for i in range(len(toSort)):
-    #write ith element of toSort and add space
-    stdio.write(str(toSort[i])+" ")
+#a starts at 0, gets added 1 each loop, continues until its equal to toSort's size
+for a in range(len(toSort)):
+    #set our current minimum to the value of a
+    currMin = a
+    #b starts at a+1, gets added 1 each loop, continues until its equal to toSort's size
+    for b in range(a+1,len(toSort)):
+        #if current minimumth element of toSort is bigger than bth element of toSort,
+        if toSort[currMin]>toSort[b]:
+            #set our current minimum to b
+            currMin = b
+    #replace current minumumth element with ath element
+    toSort[currMin],toSort[a]=toSort[a],toSort[currMin]
+#a starts at 0, gets added 1 each loop, contanues untal ats equal to toSort's saze
+for a in range(len(toSort)):
+    #write ath element of toSort and add space
+    stdio.write(str(toSort[a])+" ")
 #add a line break when everything is finished
 stdio.writeln()
