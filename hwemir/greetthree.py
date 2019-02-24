94799db121640e0f416bb2f8090a291dd5d1ee82
emirozturk011 <emirozturk011@gmail.com>
2019-02-24 15:26:11 +0300

    All documents are working. If anything is wrong let me know.

---
+++ hwemir/greetthree.py
@@ -0,0 +1,10 @@
+import sys
+import stdio
+stdio.write("Hi ")
+stdio.write(sys.argv[1])
+stdio.write(" , ")
+stdio.write(sys.argv[2])
+stdio.write(" , ")
+stdio.write(" and ")
+stdio.write(sys.argv[3])
+stdio.write(".")
